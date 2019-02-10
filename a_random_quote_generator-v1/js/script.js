94da7fce027d295be69a4fa6407363ff8ae0a005
Vinson M <vinson.mcc@gmail.com>
2019-02-09 22:22:17 -0800

    adding project files
    adding css, html, javascript, readme

---
+++ a_random_quote_generator-v1/js/script.js
@@ -0,0 +1,62 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 1 - A Random Quote Generator
+completed by Vinson
+******************************************/
+
+// Quotes
+var quotes = [
+  {
+      quote: "The important thing is not to stop questioning. Curiosity has its own reason for existence. " +
+      "One cannot help but be in awe when he contemplates the mysteries of eternity, of life, of the marvelous structure of reality. " +
+      "It is enough if one tries merely to comprehend a little of this mystery each day.",
+      citation: "Old Man's Advice to Youth: 'Never Lose a Holy Curiosity.' LIFE Magazine (2 May 1955) p. 64",
+      source: "Albert Einstein",
+      year: 1955
+  },
+  {
+      quote: "I suppose it is tempting, if the only tool you have is a hammer, to treat everything as if it were a nail.",
+      source: "Abraham Maslow",
+      citation: "Toward a Psychology of Being"
+  },
+  {
+      quote: "The truth knocks on the door and you say, \"Go away, I'm looking for the truth,\" and so it goes away. Puzzling.",
+      source: "Robert M. Pirsig",
+      citation: "Zen and the Art of Motorcycle Maintenance: An Inquiry Into Values",
+      year:  2006
+  },
+  {
+      quote: "The only true wisdom is that you know nothing",
+      source: "Socrates"
+  },
+  {
+      quote: "The eye sees only what the mind is prepared to comprehend.",
+      source: "Robertson Davies",
+      citation: "Temptest-Tost",
+      year: 1980
+  }
+]
+
+// Returns a random quote
+function getRandomQuote() {
+  var randomNum = Math.floor(Math.random()*quotes.length);  //create random index of quote
+  return quotes[randomNum];
+}
+
+// Creates an HTML string and sets innerHTML of 'quote-box' div
+function printQuote() {
+  var quoteToPrint = getRandomQuote();
+  var html = '';
+
+  //create string
+  html += '<p class="quote">' + quoteToPrint.quote+ '</p>';
+  html += '<p class="source">' + quoteToPrint.source + '</p>';
+  if ('citation' in quoteToPrint){html += '<span class="citation">' + quoteToPrint.citation + '</span>';}
+  if ('year' in quoteToPrint){html += '<span class="year">' + quoteToPrint.year + '</span>';}
+  html += '</p>';
+
+  document.getElementById('quote-box').innerHTML = html;
+}
+
+// loads new quote when button is clicked
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
